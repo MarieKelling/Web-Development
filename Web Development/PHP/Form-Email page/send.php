c6b7c3db756b48ab4f251fcff94431f5a746cb63
MarieKelling <mariekelling97@gmail.com>
2018-03-07 07:13:51 -0500

    3/7
    3/7

--- Web Development/PHP/Form-Email page/send.php
+++ Web Development/PHP/Form-Email page/send.php
@@ -1,6 +1,31 @@
 <?php
 
+	if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) )
+	{
+			$name = $_POST['name']; 									// HINT: use preg_replace() to filter the data
+			$email = $_POST['email'];
+			$message = nl2br($_POST['message']);					//Converts new lines inputted by the user to <br> tags 
+			$to = "mariekelling@yahoo.com";
+			$from = $email;
+			$subject = 'Contact Form Message';
+			$body = '<b>Name:</b> '.$name.' <br> <b>Email:</b> '.$email.'  <p>'.$message.'</p>';
+			
+			$headers = "From: $from\n";
+			$headers .= "MIME-Version: 1.0\n";
+			$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+			
+			if( mail($to, $subject, $body, $headers) ) 
+			{
+				echo "success";
+			} 
+			else 
+			{
+				
+				echo "The server failed to send the message. Please try again later.";
+			}
+	}
 	$name = $_POST['name'];
+	$email = $_POST['email']; 
 	$message = $_POST['message'];
 	
 	$to = "mariekelling@yahoo.com";
